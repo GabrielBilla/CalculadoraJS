a8fc3c51b6a940f150c48e5df0c7ffaf57b916ec
GabrielBilla <gabriel.bila@gmail.com>
2023-04-11 21:35:48 -0300

    Refatorei a funcao showTela e criei uma nova funcao checkCalcular

--- script.js
+++ script.js
@@ -7,25 +7,81 @@ botoes.forEach((botao) => {
 });
 
 function capturarClick(Button) {
-  Button.addEventListener("click",function (){ ShowTela(Button)});
+  Button.addEventListener("click", function () {
+    ShowTela(Button);
+  });
 }
 
 function ShowTela(Button) {
-    console.log(typeof Button.innerText)
-    if(Button.classList.contains("numero")){
-        tela.innerHTML += Button.innerText;
+  if (Button.classList.contains("numero")) {
+    tela.innerHTML += Button.innerText;
+  } else {
+    switch (Button.innerText) {
+      case "=":
+        calcular(tela.innerText);
+        break;
+      case "AC":
+        tela.innerHTML = "";
+        break;
+      case "√":
+        tela.innerHTML += ` ${Button.innerText} `;
+        calcular(tela.innerText);
+        break;
+      case "X²":
+        tela.innerHTML += ` ${Button.innerText} `;
+        calcular(tela.innerText);
+        break;
+      default:
+        tela.innerHTML += ` ${Button.innerText} `;
+        CheckCalcular(tela.innerText, Button.innerText);
+        break;
     }
-    else if(Button.classList.contains("operador")){
-        if(Button.innerText == "="){
-            console.log("=")
-        }
-        else if(Button.innerText == "AC"){
-          tela.innerHTML = ""
-        } 
-        else{
-            tela.innerHTML += Button.innerText;
-        }
-    }
-   
-
   }
+}
+
+function CheckCalcular(checar, botao) {
+  var word = checar.split(" ");
+  console.log(word);
+  if (word[2] && !word[3])  {
+     let botoesOperador = document.querySelectorAll(".operador")
+    botoesOperador.forEach((botao) => {
+      if (botao.innerText == word[2]) {
+        word[1] = word[2];
+        tela.innerHTML = `${word[0]}  ${word[1]} `
+      }
+    });
+  }else if(word[3]){
+    calcular(tela.innerText)
+  }
+}
+
+function calcular(calculo) {
+  var word = calculo.split(" ");
+  var num1 = Number(word[0]);
+  var operador = word[1];
+  var num2 = Number(word[2]);
+  if (!num2) {
+    num2 = num1;
+  }
+
+  switch (operador) {
+    case "+":
+      tela.innerHTML = num1 + num2;
+      break;
+    case "-":
+      tela.innerHTML = num1 - num2;
+      break;
+    case "*":
+      tela.innerHTML = num1 * num2;
+      break;
+    case "/":
+      tela.innerHTML = num1 / num2;
+      break;
+    case "√":
+      tela.innerHTML = Math.sqrt(num1);
+      break;
+    case "X²":
+      tela.innerHTML = Math.pow(num1, 2);
+      break;
+  }
+}
